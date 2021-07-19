839633e5729219c5aa217640ff07db90f6aaf429
idjaeha <jaehanim1@gmail.com>
2021-07-20 03:08:23 +0900

    VSTR-52 QR 코드 생성 페이지 추가

--- src/components/QR.js
+++ src/components/QR.js
@@ -1,42 +1,27 @@
-import { Typography, Button, makeStyles, Box } from "@material-ui/core";
+import { makeStyles, Box } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import QRCode from "qrcode";
 import styles from "styles/QR.module.css";
-import { decrypt } from "tools/dataHandler";
-import phoneImg from "images/phone.png";
-import msg from 'images/msg.png';
 
-const useStyles = makeStyles(
-  {
-    phoneBox: {
-      width: "500px",
-      height: "500px",
-    },
-    phone: {
-      width: "100%",
-      height: "100%",
-      objectFit: "cover",
-    },
-    msgBox: {
-      width: "200px",
-      height: "200px",
-      position: "absolute",
-      display: "flex",
-      flexDirection: "column",
-      left: "175px",
-      top: "80px"
-    },
-    msgImg: {
-      width: "100%",
-      height: "100%",
-      objectFit: "cover",
-    }
+const useStyles = makeStyles({
+  qrBox: {
+    width: "100%",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  errorBox: {
+    width: "50vw",
+    height: "50vw",
+    fontSize: "2.5rem",
+    background: "white",
+    textAlign: "center"
   }
-)
+});
 
 const QR = (props) => {
   const [QR, setQR] = useState("");
-  const [openMsg, setOpenMsg] = useState(false);
 
   const classes = useStyles();
 
@@ -50,43 +35,21 @@ const QR = (props) => {
   };
 
   useEffect(() => {
-    let data = "";
-    if (props.location !== undefined) {
-      data = props.location.search.slice(6);
-      data = JSON.stringify(decrypt(data, process.env.REACT_APP_AES_KEY));
-    }
-    else if (props.code !== undefined) {
-      data = JSON.stringify(decrypt(props.code, process.env.REACT_APP_AES_KEY));
-    }
-    generateQR(data);
-  }, [props.location, props.code]);
-
-  const handleOpenMsgClick = () => {
-    setOpenMsg(true);
-  }
+    const {
+      match: {
+        params: { code },
+      },
+    } = props;
+    generateQR(code);
+  }, [props]);
 
   return (
-    <Box className={classes.phoneBox}>
-      <img src={phoneImg} alt="phone" className={classes.phone} />
-      {openMsg === false ?
-        <Box className={classes.msgBox}>
-          <Typography variant="h3">방문객</Typography>
-          <Typography variant="subtitle2">메세지가 도착했습니다!</Typography>
-          <Button onClick={handleOpenMsgClick}>
-            <img src={msg} alt="msg" className={classes.msgImg} />
-          </Button>
-        </Box>
-        :
-        <Box className={classes.msgBox}>
-          <Typography variant="h3">방문 확인</Typography>
-          <Typography variant="subtitle2">QR 리더기에 보여주세요.</Typography>
-          {QR === "" ? (
-            <Typography variant="h5">잘못된 접근입니다.</Typography>
-          ) : (
-            <img src={QR} alt="QRCode" className={styles.QR} />
-          )}
-        </Box>
-      }
+    <Box className={classes.qrBox}>
+      {QR === "" ? (
+        <Box className={classes.errorBox}>주소가 잘못되었습니다.</Box>
+      ) : (
+        <img src={QR} alt="QRCode" className={styles.qrImg} />
+      )}
     </Box>
   );
 };
