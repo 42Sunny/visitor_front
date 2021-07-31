7b536cf2be2d49903ca917a02933f478430fc18e
idjaeha <jaehanim1@gmail.com>
2021-07-31 17:41:25 +0900

    구버전 제거

--- README.md
+++
@@ -1,5 +0,0 @@
-# visitor_front
-- 이노베이션 아카데미 방문자 출입 관리 시스템입니다.
-
-# 링크
-- https://visitor.dev.42seoul.io/#/
--- src/routes/Reservation.js
+++
@@ -1,8 +0,0 @@
-import ApplicationContent from "components/ApplicationContent";
-import React from "react";
-
-const Reservation = () => {
-  return <ApplicationContent />;
-};
-
-export default Reservation;
