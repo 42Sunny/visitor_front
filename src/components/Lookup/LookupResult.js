6ae23ec586d581d12206462e81b3e64dea48a0a3
idjaeha <jaehanim1@gmail.com>
2021-08-10 16:00:36 +0900

    VSTR-63 예약 삭제 시, 확인 팝업 추가

--- src/components/Lookup/LookupResult.js
+++ src/components/Lookup/LookupResult.js
@@ -1,9 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import icon_no_result from 'images/icon_no_result.svg';
 import styles from 'styles/LookupPage.module.css';
 import { LookupContext } from 'contexts/LookupContext';
 import { deleteReserve } from 'tools/apiHandler';
 import { useHistory } from 'react-router-dom';
+import ReactModal from 'react-modal';
+
+const LookupDeleteModal = ({
+  isOpen,
+  onRequestClose,
+  onDeleteButtonClick,
+  onCancelButtonClick,
+}) => {
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      className={styles.LookupDeleteModal}
+      ariaHideApp={false}
+    >
+      <div className={styles.LookupDeleteModalBox}>
+        <div className={styles.LookupDeleteModalHeader}>
+          <div>예약을</div>
+          <div>삭제하시겠습니까?</div>
+        </div>
+        <div className={styles.LookupDeleteModalContent}>
+          <button onClick={onDeleteButtonClick} className={styles.LookupDeleteModalDeleteButton}>
+            삭제
+          </button>
+          <button onClick={onCancelButtonClick} className={styles.LookupDeleteModalCancelButton}>
+            취소
+          </button>
+        </div>
+      </div>
+    </ReactModal>
+  );
+};
 
 const LookupNoResult = () => {
   return (
@@ -18,15 +50,28 @@ const LookupNoResult = () => {
 
 const LookupCard = ({ place, targetStaffName, purpose, date, reserveId, result }) => {
   const { reserve, setReserve } = useContext(LookupContext);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const history = useHistory();
 
   const handleDeleteClick = () => {
+    handleModalOpen();
+  };
+
+  const handleDelete = () => {
     const name = result.visitor[0].name;
     const phone = result.visitor[0].phone;
     deleteReserve(name, phone, reserveId).then(() => {
       const newReserve = reserve.filter((res) => res.visitor[0].reserveId !== reserveId);
       setReserve(newReserve);
     });
+  };
+
+  const handleModalClose = () => {
+    setIsDeleteModalOpen(false);
+  };
+
+  const handleModalOpen = () => {
+    setIsDeleteModalOpen(true);
   };
 
   const handleUpdateClick = () => {
@@ -37,34 +82,42 @@ const LookupCard = ({ place, targetStaffName, purpose, date, reserveId, result }
   };
 
   return (
-    <div className={styles.LookupCardBox}>
-      <div className={styles.LookupCardContent}>
-        <div className={styles.LookupCardRow}>
-          <div className={styles.LookupCardRowTitle}>방문 날짜</div>
-          <div className={styles.LookupCardRowContent}>{date}</div>
+    <>
+      <div className={styles.LookupCardBox}>
+        <div className={styles.LookupCardContent}>
+          <div className={styles.LookupCardRow}>
+            <div className={styles.LookupCardRowTitle}>방문 날짜</div>
+            <div className={styles.LookupCardRowContent}>{date}</div>
+          </div>
+          <div className={styles.LookupCardRow}>
+            <div className={styles.LookupCardRowTitle}>장소</div>
+            <div className={styles.LookupCardRowContent}>{place}</div>
+          </div>
+          <div className={styles.LookupCardRow}>
+            <div className={styles.LookupCardRowTitle}>방문 대상</div>
+            <div className={styles.LookupCardRowContent}>{targetStaffName}</div>
+          </div>
+          <div className={styles.LookupCardRow}>
+            <div className={styles.LookupCardRowTitle}>방문 목적</div>
+            <div className={styles.LookupCardRowContent}>{purpose}</div>
+          </div>
         </div>
-        <div className={styles.LookupCardRow}>
-          <div className={styles.LookupCardRowTitle}>장소</div>
-          <div className={styles.LookupCardRowContent}>{place}</div>
-        </div>
-        <div className={styles.LookupCardRow}>
-          <div className={styles.LookupCardRowTitle}>방문 대상</div>
-          <div className={styles.LookupCardRowContent}>{targetStaffName}</div>
-        </div>
-        <div className={styles.LookupCardRow}>
-          <div className={styles.LookupCardRowTitle}>방문 목적</div>
-          <div className={styles.LookupCardRowContent}>{purpose}</div>
+        <div className={styles.LookupCardButtons}>
+          <button className={styles.LookupCardDeleteButton} onClick={handleDeleteClick}>
+            삭제
+          </button>
+          <button className={styles.LookupCardUpdateButton} onClick={handleUpdateClick}>
+            수정
+          </button>
         </div>
       </div>
-      <div className={styles.LookupCardButtons}>
-        <button className={styles.LookupCardDeleteButton} onClick={handleDeleteClick}>
-          삭제
-        </button>
-        <button className={styles.LookupCardUpdateButton} onClick={handleUpdateClick}>
-          수정
-        </button>
-      </div>
-    </div>
+      <LookupDeleteModal
+        isOpen={isDeleteModalOpen}
+        onRequestClose={handleModalClose}
+        onDeleteButtonClick={handleDelete}
+        onCancelButtonClick={handleModalClose}
+      />
+    </>
   );
 };
 
