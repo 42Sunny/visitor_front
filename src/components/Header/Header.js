0e08e50eb7e6d6b868a03523a0a810729baad6d4
idjaeha <jaehanim1@gmail.com>
2021-09-13 18:27:16 +0900

    VSTR-109 Navigation Bar 로직 변경

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles, { selected } from './Header.module.css';
 import { useHistory, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
 
 const RESERVE_PATH = '/reserve';
 const LOOKUP_PATH = '/lookup';
+const RESERVE_INFO_PATH = '/reserve-info';
+const QR_PATH = '/qr';
 
 const Header = () => {
   const location = useLocation();
-  const { pathname } = location;
   const history = useHistory();
+  const { pathname } = location;
+  const [basePath, setBasePath] = useState('');
+
+  useEffect(() => {
+    const basePath = `/${pathname.split('/')[1]}`;
+    setBasePath(basePath);
+  }, [pathname]);
 
   return (
     <div className={styles.Header}>
       <div className={styles.LogoText}>Innovation Academy</div>
       <div className={styles.TitleBox}>
         <div
-          className={classNames(styles.Title, { [selected]: pathname === RESERVE_PATH })}
+          className={classNames(styles.Title, { [selected]: basePath === RESERVE_PATH })}
           onClick={() => {
             history.push(RESERVE_PATH);
           }}
@@ -24,13 +32,23 @@ const Header = () => {
           {!location.state ? '방문 예약' : '예약 수정'}
         </div>
         <div
-          className={classNames(styles.Title, { [selected]: pathname === LOOKUP_PATH })}
+          className={classNames(styles.Title, { [selected]: basePath === LOOKUP_PATH })}
           onClick={() => {
             history.push(LOOKUP_PATH);
           }}
         >
           예약 조회
         </div>
+        {basePath === RESERVE_INFO_PATH && (
+          <div className={classNames(styles.Title, { [selected]: basePath === RESERVE_INFO_PATH })}>
+            상세 내역
+          </div>
+        )}
+        {basePath === QR_PATH && (
+          <div className={classNames(styles.Title, { [selected]: basePath === QR_PATH })}>
+            QR 체크인
+          </div>
+        )}
       </div>
     </div>
   );
