0ecc3b4f847fb534b0aee3376138fb25dac26fae
idjaeha <jaehanim1@gmail.com>
2021-09-14 03:28:19 +0900

    VSTR-109 그림자, 네비게이션 바 위치 조정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styles, { selected } from './Header.module.css';
 import { useHistory, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
+import logo from 'images/bi_img02.png';
 
 const RESERVE_PATH = '/reserve';
 const LOOKUP_PATH = '/lookup';
@@ -21,34 +22,40 @@ const Header = () => {
 
   return (
     <div className={styles.Header}>
-      <div className={styles.LogoText}>Innovation Academy</div>
-      <div className={styles.TitleBox}>
-        <div
-          className={classNames(styles.Title, { [selected]: basePath === RESERVE_PATH })}
-          onClick={() => {
-            history.push(RESERVE_PATH);
-          }}
-        >
-          {!location.state ? '방문 예약' : '예약 수정'}
-        </div>
-        <div
-          className={classNames(styles.Title, { [selected]: basePath === LOOKUP_PATH })}
-          onClick={() => {
-            history.push(LOOKUP_PATH);
-          }}
-        >
-          예약 조회
-        </div>
-        {basePath === RESERVE_INFO_PATH && (
-          <div className={classNames(styles.Title, { [selected]: basePath === RESERVE_INFO_PATH })}>
-            상세 내역
+      <div className={styles.Content}>
+        <div className={styles.TitleBox}>
+          <div
+            className={classNames(styles.Title, { [selected]: basePath === RESERVE_PATH })}
+            onClick={() => {
+              history.push(RESERVE_PATH);
+            }}
+          >
+            {!location.state ? '방문 예약' : '예약 수정'}
           </div>
-        )}
-        {basePath === QR_PATH && (
-          <div className={classNames(styles.Title, { [selected]: basePath === QR_PATH })}>
-            QR 체크인
+          <div
+            className={classNames(styles.Title, { [selected]: basePath === LOOKUP_PATH })}
+            onClick={() => {
+              history.push(LOOKUP_PATH);
+            }}
+          >
+            예약 조회
           </div>
-        )}
+          {basePath === RESERVE_INFO_PATH && (
+            <div
+              className={classNames(styles.Title, { [selected]: basePath === RESERVE_INFO_PATH })}
+            >
+              상세 내역
+            </div>
+          )}
+          {basePath === QR_PATH && (
+            <div className={classNames(styles.Title, { [selected]: basePath === QR_PATH })}>
+              QR 체크인
+            </div>
+          )}
+        </div>
+        <div className={styles.LogoBox}>
+          <img className={styles.LogoImg} src={logo} alt="logo" />
+        </div>
       </div>
     </div>
   );
