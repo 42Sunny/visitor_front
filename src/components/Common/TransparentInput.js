f5744eaeb60adcf46144a2658a507937c19aee90
idjaeha <jaehanim1@gmail.com>
2021-10-14 16:12:25 +0900

    VSTR-145 예약 수정 시, 직원 이름 오류 해결
    - 직원 이름을 보이게 만드는 컴포넌트의 props가 제대로 전달되지 않음

--- src/components/Common/TransparentInput.js
+++ src/components/Common/TransparentInput.js
@@ -2,12 +2,13 @@ import classNames from 'classnames';
 import React from 'react';
 import classes from 'styles/Common/TransparentInput.module.css';
 
-const TransparentInput = ({ className, onChange, placeholder }) => {
+const TransparentInput = ({ className, onChange, placeholder, value }) => {
   return (
     <input
       className={classNames(className, classes.input)}
       onChange={onChange}
       placeholder={placeholder}
+      value={value}
     />
   );
 };
