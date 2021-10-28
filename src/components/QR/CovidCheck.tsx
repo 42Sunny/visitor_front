ecb73d5fff68c4ab8b02e75bdfc7791b3f9c20bd
idjaeha <jaehanim1@gmail.com>
2021-10-28 20:33:49 +0900

    VSTR-164 QR체크인에 모두 동의 항목 추가
    사용자의 편의성을 위해 모두 동의 항목을 추가하고,
    체크 박스의 위치를 타 서비스와 동일하게 왼쪽으로 변경했습니다.

--- src/components/QR/CovidCheck.tsx
+++ src/components/QR/CovidCheck.tsx
@@ -3,11 +3,14 @@ import classes from 'assets/styles/QR/CovidCheck.module.css';
 import Spacing from 'components/Common/Spacing';
 import QRCode from './QRCode';
 import QRMessage from './QRMessage';
+import classNames from 'classnames';
 
 const TEMP_CONTENT = '발열 체크시 37.5도 이하인 것을 확인했습니다.';
 const MASK_CONTENT = '마스크를 반드시 상시 착용하고 방역수칙을 준수할 것을 약속합니다.';
+const ALL_CONTENT = '모두 동의합니다.';
 
 const CovidCheck = () => {
+  const [allCheck, setAllCheck] = useState(false);
   const [maskCheck, setMaskCheck] = useState(false);
   const [tempCheck, setTempCheck] = useState(false);
   const [check, setCheck] = useState(0);
@@ -20,23 +23,39 @@ const CovidCheck = () => {
     if (check !== 2) setMaskCheck(!maskCheck);
   };
 
+  const handleAllClick = () => {
+    setAllCheck(!allCheck);
+    if (check !== 2) {
+      setTempCheck(!allCheck);
+      setMaskCheck(!allCheck);
+    }
+  };
+
   useEffect(() => {
     let num = maskCheck === true ? 1 : 0;
     num += tempCheck === true ? 1 : 0;
+    setAllCheck(num === 2);
     setCheck(num);
   }, [maskCheck, tempCheck]);
 
   return (
     <div className={classes.Container}>
       <QRCode
-        className={
-          check === 0 ? classes.FullBlur : check === 1 ? classes.HalfBlur : classes.QRContainer
-        }
+        className={check === 0 ? classes.FullBlur : check === 1 ? classes.HalfBlur : undefined}
       />
       <QRMessage isChecked={check === 2} />
       <Spacing one />
+      <div className={classes.Box} onClick={handleAllClick}>
+        <input
+          type="checkbox"
+          className={classes.CheckBox}
+          checked={allCheck}
+          onChange={handleAllClick}
+          disabled={check === 2}
+        />
+        <div className={classNames(classes.Content, classes.Bold)}>{ALL_CONTENT}</div>
+      </div>
       <div className={classes.Box} onClick={handleMaskClick}>
-        <div className={classes.Content}>{MASK_CONTENT}</div>
         <input
           type="checkbox"
           className={classes.CheckBox}
@@ -44,9 +63,9 @@ const CovidCheck = () => {
           onChange={handleMaskClick}
           disabled={check === 2}
         />
+        <div className={classes.Content}>{MASK_CONTENT}</div>
       </div>
       <div className={classes.Box} onClick={handleTempClick}>
-        <div className={classes.Content}>{TEMP_CONTENT}</div>
         <input
           type="checkbox"
           className={classes.CheckBox}
@@ -54,6 +73,7 @@ const CovidCheck = () => {
           onChange={handleTempClick}
           disabled={check === 2}
         />
+        <div className={classes.Content}>{TEMP_CONTENT}</div>
       </div>
       <Spacing two />
     </div>
