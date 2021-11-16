05fac5ac1567dcc806e83cc5bdeb041d1b1f1c48
idjaeha <jaehanim1@gmail.com>
2021-11-16 15:16:47 +0900

    VSTR-179 로컬 환경에서 이슈가 등록되지 않도록 설정
    로컬 환경에서는 개발이 계속해서 이뤄지기 때문에 그에 대한 에러가
    발생한다.
    이러한 에러를 sentry에서 전부 감지하는 것은 적절하지 못할 뿐 아니라
    제한된 이슈 개수를 넘길 수 있게 되기 때문에 로컬 환경에서는 이슈가
    등록되지 않도록 설정한다.

--- src/tools/initSentry.ts
+++ src/tools/initSentry.ts
@@ -2,16 +2,18 @@ import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
 
 const initSentry = () => {
-  Sentry.init({
-    dsn: process.env.REACT_APP_SENTRY_DSN,
-    integrations: [new Integrations.BrowserTracing()],
-    environment: process.env.REACT_APP_ENVIRONMENT,
+  if (process.env.REACT_APP_ENVIRONMENT !== 'local') {
+    Sentry.init({
+      dsn: process.env.REACT_APP_SENTRY_DSN,
+      integrations: [new Integrations.BrowserTracing()],
+      environment: process.env.REACT_APP_ENVIRONMENT,
 
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-  });
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: 1.0,
+    });
+  }
 };
 
 export default initSentry;
