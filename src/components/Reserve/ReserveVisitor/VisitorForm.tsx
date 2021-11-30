ddafdb45e208995d0690dc0dfa9aa421fb7173fe
idjaeha <jaehanim1@gmail.com>
2021-11-30 14:03:00 +0900

    VSTR-190 방문자 번호에 공백이 입력되는 현상 제거
    isNaN은 " "의 경우 0으로 변환하여 반환값을 false로 내보냅니다.
    때문에 공백의 경우 해당 로직에 따르면 숫자로 인식되기 때문에 해당 부분을
    수정합니다.

--- src/components/Reserve/ReserveVisitor/VisitorForm.tsx
+++ src/components/Reserve/ReserveVisitor/VisitorForm.tsx
@@ -4,6 +4,7 @@ import React, { useCallback } from 'react';
 import VisitorInput from './VisitorInput';
 import classes from 'assets/styles/Reserve/ReserveVisitor.module.css';
 import { convertPhone } from 'hooks/useVisitors';
+import isNumber from 'tools/isNumber';
 
 const NAME_ORGANIZATION = 'organization';
 const NAME_NAME = 'name';
@@ -27,7 +28,7 @@ type PropTypes = {
   saveVisitor: () => void;
 };
 
-const isPhoneCharacter = (ch: any) => isNaN(ch) === false || ch === '-';
+const isPhoneCharacter = (ch: string) => ch === '-' || isNumber(ch);
 
 const VisitorForm = ({ visitor, deleteVisitor, saveVisitor }: PropTypes) => {
   const handleSave = () => {
