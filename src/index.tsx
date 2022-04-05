edaab20b5a3c66a0d7c54856cb3790efc1c2e251
idjaeha <jaehanim1@gmail.com>
2022-04-05 11:58:46 +0900

    [#7] Github action으로 배포 자동화

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import initSentry from 'tools/initSentry';
 import App from './components/App';
-
-initSentry();
 
 ReactDOM.render(<App />, document.getElementById('root'));
